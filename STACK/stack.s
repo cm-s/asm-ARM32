453efcb1e867b9a062a5f91b388076497e183e11
cm-s <blufyr3530@gmail.com>
2017-01-05 07:16:08 -0800

    further explained stack

--- STACK/stack.s
+++ STACK/stack.s
@@ -8,14 +8,18 @@ _start:
 	MOV R1, #1
 	MOV R2, #2
 
-	STR R1, [sp, #-4]! 	@ POINT FOUR BITS DOWN 
-	STR R2, [sp, #-4]! 	@ POINT FOUR BITS DOWN
+	STR R1, [sp, #-4]! 	@ LOAD R1 (#1)
+				@ POINT FOUR BITS DOWN 
+	STR R2, [sp, #-4]! 	@ LOAD R2 (#2)
+				@ POINT FOUR BITS DOWN
 
-	LDR R0, [sp], #+4 	@ LOAD VALUE & POINT UP
-	LDR R0, [sp], #+4 	@ LOAD VALUE & POINT UP
+	LDR R0, [sp], #+4 	@ LOAD R2's VALUE (#2) 
+				@ POINT FOUR BITS UP
+	LDR R0, [sp], #+4 	@ LOAD R1's VALUE (#1)
+				@ POINT FOUR BITS UP
 
 end:
 	MOV R7, #1
-	SWI 0
+	SWI 0			@ `./stack.run ; echo $?` == 1
 	
 	
